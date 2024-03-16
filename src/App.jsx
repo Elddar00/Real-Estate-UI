d31065d467a2905446b663cf26f0ebb12d0e3608
Elddar00 <eldaridrizovic00@gmail.com>
2024-03-16 03:56:04 +0100

    Responsive Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
+import "./layout.scss";
+
 function App() {
-  return (
-    <div>
-      <a href="/">Hello</a>
-    </div>
-  );
+  return <div className="layout">Hello</div>;
 }
 
 export default App;
