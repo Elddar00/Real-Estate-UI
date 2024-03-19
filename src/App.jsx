fb26eb3d25932fe3ef3419c02af5cec8045757c2
Elddar00 <eldaridrizovic00@gmail.com>
2024-03-19 15:02:18 +0100

    Implement React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
 import Navbar from "./components/navbar/Navbar";
 import "./layout.scss";
 import HomePage from "./routes/homePage/homePage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import ListPage from "./routes/homePage/listPage/listPage";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+    {
+      path: "/list",
+      element: <ListPage />,
+    },
+  ]);
+
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
+    // <div className="layout">
+    //   <div className="navbar">
+    //     <Navbar />
+    //   </div>
+    //   <div className="content">
+    //     <HomePage />
+    //   </div>
+    // </div>
+    <RouterProvider router={router} />
   );
 }
 
