b507ae48686f985b91b7adfe7a6924876db13ddb
Elddar00 <eldaridrizovic00@gmail.com>
2024-03-20 16:05:27 +0100

    Responsive List Page

--- src/routes/homePage/listPage/listPage.jsx
+++ src/routes/homePage/listPage/listPage.jsx
@@ -1,7 +1,15 @@
+import { listData } from "../../../lib/dummydata";
 import "./listPage.scss";
 
 function ListPage() {
-  return <div>listPage</div>;
+  const data = listData;
+
+  return (
+    <div className="listPage">
+      <div className="listContainer">List</div>
+      <div className="mapContainer">Map</div>
+    </div>
+  );
 }
 
 export default ListPage;
