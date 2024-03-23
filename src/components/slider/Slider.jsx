d460613f70aea1b078d8922ba94ea3b98e4fcfe5
Elddar00 <eldaridrizovic00@gmail.com>
2024-03-23 23:09:54 +0100

    Slider: Added images and styled them, preparing for Slider implementation

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,7 +1,18 @@
 import "./slider.scss";
 
-function slider() {
-  return <div>Slider</div>;
+function slider({ images }) {
+  return (
+    <div className="slider">
+      <div className="bigImage">
+        <img src={images[0]} alt="" />
+      </div>
+      <div className="smallImage">
+        {images.slice(1).map((image, index) => (
+          <img src={image} alt="" key={index} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default slider;
